ef73b6be561546533fb8c5a05a7ba98ee69501df
alnezt <157808458+alnezt@users.noreply.github.com>
2026-07-23 10:54:02 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int idxLeft=0;
+        int idxRight=s.length()-1;
+        char left,right;
+        while(idxLeft<idxRight)
+        {
+            left=s.charAt(idxLeft);
+            left=Character.toLowerCase(left);
+           right=s.charAt(idxRight);
+            right=Character.toLowerCase(right);
+            while(!Character.isLetterOrDigit(left) && idxLeft<idxRight)
+            {
+                ++idxLeft;
+                left=s.charAt(idxLeft);
+                left=Character.toLowerCase(left);
+                
+            }
+              while(!Character.isLetterOrDigit(right) && idxLeft<idxRight)
+            {
+               
+                right=s.charAt(--idxRight);
+                right=Character.toLowerCase(right);
+                
+            }
+            if(left!=right)
+            {
+                return false;
+            }
+            idxLeft++;
+            idxRight--;
+        }
+        return true;
+    }
+}
